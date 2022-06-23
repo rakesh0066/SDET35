edbeb48010623927ed1fc8d523fad6df16be83c9
Rakesh <Rakesh@LAPTOP-6C7PHFIK>
2022-06-23 17:40:25 +0530

    commit6

--- project.crm.sdet35/src/main/java/Com/Crm/GenericUtilities/BaseClass.java
+++ project.crm.sdet35/src/main/java/Com/Crm/GenericUtilities/BaseClass.java
@@ -39,7 +39,7 @@ public class BaseClass {
     	String BROWSER = fLib.getPropertyKeyValue("browser");
     	String URL = fLib.getPropertyKeyValue("url");
     	
-    	//parameter jenkins  
+    
     	
  //   	 String BROWSER = System.getProperty("browser");
  //   	 String URL = System.getProperty("url");
